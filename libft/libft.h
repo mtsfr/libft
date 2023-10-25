64ffa388139a3871baf27ac3ddbc6456d88a4546
mtsfr <matheus_fborges@hotmail.com>
2023-10-24 22:02:29 -0300

    add strjoin

--- libft/libft.h
+++ libft/libft.h
@@ -48,5 +48,6 @@ char	*ft_strnstr(const char *big, const char *little, size_t len);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
